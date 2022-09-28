4bb29c07348677e5445caf275ca9ecef8125c104
ShuEugene <shueugene@hotmail.com>
2022-09-28 14:42:58 +0300

    HW8.1 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 //  Домашние задания к Уроку №8 «Методы»
 public class Main {
 
-    static void task1(){
+    static void task1(int arg) {
+        System.out.printf("%d - %s год.", arg, yearIsLeap(arg));
+    }
 
+    static String yearIsLeap(int year) {
+        if (year > 0) {
+            if (((year % 4) == 0 && (year % 100) != 0) || (year % 400) == 0) {
+                return "високосный";
+            } else return "не високосный";
+        }
+        throw new RuntimeException("Год введён некорректно.");
     }
 
     public static void main(String[] args) {
-        task1();
+        System.out.println();
+        task1(2022);
+        System.out.println();
+
     }
 }
